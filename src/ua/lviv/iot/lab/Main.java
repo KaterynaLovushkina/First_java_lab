87b0561b7c57bc0281cafe922061e9054db2d63f
KaterynaLovushkina <katerina...@MacBook-Air-Katerina.local>
2022-03-29 21:03:59 +0300

    add lab 2 code

---
+++ src/ua/lviv/iot/lab/Main.java
@@ -0,0 +1,34 @@
+package ua.lviv.iot.lab;
+
+import ua.lviv.iot.lab.classmembers.CastMember;
+import ua.lviv.iot.lab.classmembers.FilmDirector;
+import ua.lviv.iot.lab.classmembers.Producer;
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        PositionInformation[] position=new PositionInformation[6];
+
+        position[0]=new CastMember("Petro", 40,  "master", 15, 350, 35, "operator");
+        position[1]=new CastMember("Lucy", 30,  "bachelor", 3, 200, 40, "desgnerir");
+        position[2]=new CastMember("Andriy", 54,  "master", 22, 500, 48, "actor");
+        position[3]=new CastMember("Olena", 35,  "master", 7, 250, 48, "actress");
+        position[4]=new FilmDirector("John", 33,  "master", 9, 450, 40);
+        position[5] =new Producer("Stepan", 50,  "master", 23, 500, 40, "Consulting producer");
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the method you would like to choice: ");
+        System.out.println("1. Sort array by name in order");
+        System.out.println("2. Sort array by name in reverse order");
+        System.out.println("3. Sort array by work experience in order");
+        System.out.println("4. Sort array by work experience in reverse order");
+        System.out.println("Please make your choice:");
+        int sortType = sc.nextInt();
+        FilmManager.sortMembers(sortType,position);
+
+
+    }
+}
---
+++ src/ua/lviv/iot/lab/classmembers/Producer.java
@@ -0,0 +1,27 @@
+package ua.lviv.iot.lab.classmembers;
+
+import ua.lviv.iot.lab.PositionInformation;
+
+public class Producer extends PositionInformation {
+    protected String typeOfProducer;
+
+    public Producer(String name, int age,
+                    String educationDegree, int workExperience,
+                    int salaryInUAHperHour, int workingHoursPerDay,String typeOfProducer) {
+        super(name, age,  educationDegree, workExperience, salaryInUAHperHour, workingHoursPerDay);
+        this.typeOfProducer=typeOfProducer;
+    }
+    @Override
+    public String toString() {
+        return "Film Producer{" +
+                "Name:" + name + '\'' +
+                ", Age:" + age +
+                ", EducationDegree:" + educationDegree + '\'' +
+                ", WorkExperience:" + workExperience +
+                ", Email:" + name +"@com"+ '\'' +
+                ", Salary:" + salaryInUAHperHour +
+                ", WorkingHoursPerDay:" + workingHoursPerDay +
+                ", Producer type:" + typeOfProducer +
+                '}';
+    }
+}
