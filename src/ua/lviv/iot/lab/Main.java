4afad24d7bac7e1b70355823d1b9860db77c5c5e
KaterynaLovushkina <katerina...@MacBook-Air-Katerina.local>
2022-04-06 16:29:26 +0300

    add code for lab 3

---
+++ src/ua/lviv/iot/lab/Main.java
@@ -0,0 +1,29 @@
+package ua.lviv.iot.lab;
+
+import ua.lviv.iot.lab.manager.Manager;
+
+public class Main {
+    public static void main(String[] args) {
+        //DataBase.getMembers().forEach(System.out::println);
+        Manager manager=new Manager();
+        manager.findMembersForFilm("actress");
+        manager.findMembersForFilm("actor");
+        manager.findMembersForFilm("operator");
+        manager.findMembersForFilm("art director");
+        manager.findMembersForFilm("producer");
+
+        System.out.println("-------------------------------------------------Staff for Documentary Film-----------------------------------------");
+        manager.getStaffForFIlm().forEach(System.out::println);
+        System.out.println();
+        System.out.println("-------------------------------------------------Staff for Documentary Film sorted by Work Experience-----------------------------------------");
+        manager.sortByWorkExperience(false).forEach(System.out::println);
+        System.out.println();
+        System.out.println("-------------------------------------------------Staff for Documentary Film sorted by Age-----------------------------------------");
+        manager.sortByAge(true).forEach(System.out::println);
+
+
+
+
+
+    }
+}
