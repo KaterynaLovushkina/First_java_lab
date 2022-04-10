4a9b6bfd73dd4358c92827ac3e2ce17aab6b338b
KaterynaLovushkina <katerina...@MacBook-Air-Katerina.local>
2022-04-10 17:02:09 +0300

    add lab 4 code

---
+++ src/main/java/ua/lviv/iot/lab4/models/MemberForTemporaryJob.java
@@ -0,0 +1,35 @@
+package ua.lviv.iot.lab3.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberForTemporaryJob extends PositionInformation {
+    private int salaryInUAHperHour;
+    private int workingHoursPerDay;
+
+    public MemberForTemporaryJob(String occupation, String name, int age,
+                                 String educationDegree, int workExperience,
+                                 int salaryInUAHperHour, int workingHoursPerDay) {
+        super(occupation, name, age, educationDegree, workExperience);
+        this.salaryInUAHperHour = salaryInUAHperHour;
+        this.workingHoursPerDay = workingHoursPerDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                " Occupation:'" + this.getOccupation() + '\'' +
+                ", Name:'" + this.getName() + '\'' +
+                ", Age:" + this.getAge() +
+                ", EducationDegree:'" + this.getEducationDegree() + '\'' +
+                ", WorkExperience:" + this.getWorkExperience() +
+                " Salary per hour:" + salaryInUAHperHour + " UAH" +
+                ", WorkingHoursPerDay:" + workingHoursPerDay +
+                '}';
+
+    }
+}
