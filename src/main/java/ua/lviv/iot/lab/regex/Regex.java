0c49067bef867684c2ec1e8e26333fdf04430037
KaterynaLovushkina <Kateryna.Lovushkina.IR.2021@lpnu.ua>
2022-05-10 20:10:28 +0300

    add modified lab 7 code

--- src/main/java/ua/lviv/iot/lab/regex/Regex.java
+++ src/main/java/ua/lviv/iot/lab/regex/Regex.java
@@ -1,7 +1,14 @@
 package ua.lviv.iot.lab.regex;
 
-import java.io.*;
-import java.util.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Regex {
@@ -10,35 +17,36 @@ public class Regex {
         this.path=path;
     }
     public String[] getTextFromFile() throws IOException {
-        String[] SENTENCE = new String[0];
+        String[] sentances = new String[0];
         File file=new File(this.path);
 
-        Scanner sentence = new Scanner(file);
+        Scanner text = new Scanner(file);
         ArrayList<String> sentenceList = new ArrayList<String>();
-        while (sentence.hasNextLine()) {
-            sentenceList.add(sentence.nextLine());
+        while (text.hasNextLine()) {
+            sentenceList.add(text.nextLine());
         }
-        sentence.close();
+        text.close();
         String[] sentenceArray = sentenceList.toArray(new String[0]);
         for (int r = 0; r < sentenceArray.length; r++) {
-            SENTENCE = sentenceArray[r].split("(?<=[.!?])\\s*"); //split sentences and store in array
+            sentances = sentenceArray[r].split("(?<=[.!?])\\s*");
         }
-        for (int i = 0; i < SENTENCE.length; i++) {
-            System.out.println("Sentence " + (i + 1) + ": " + SENTENCE[i]);
+        for (int i = 0; i < sentances.length; i++) {
+            System.out.println("Sentence " + (i + 1) + ": " + sentances[i]);
         }
-        return SENTENCE;
+        return sentances;
     }
 
     public List<String> readFile() throws IOException {
         List<String> listOfText=new ArrayList<>();
         String[] text = getTextFromFile();
         List longestWordsList=findLongestWord();
-        Pattern pattern = Pattern.compile("\\b[aieuoAIEOU]\\w+");
+        Pattern pattern = Pattern.compile("\\b(?i)[aieuo]\\w+");
         String str = "";
         int i=0;
         for (String s3 : text) {
             while(i<longestWordsList.size()) {
-                str = s3.replaceFirst(pattern.pattern(), (String) longestWordsList.get(i));
+                str = s3.replaceFirst(pattern.pattern(),
+                                      (String)  longestWordsList.get(i));
                 listOfText.add(str);
                 i++;
                 break;
@@ -54,7 +62,8 @@ public class Regex {
 
         for (String words:text) {
             List<String> strings = Arrays.asList(words.split(" "));
-            String biggestWord = Collections.max(strings, Comparator.comparing(String::length));
+            String biggestWord = Collections.max(strings,
+                                                 Comparator.comparing(String::length));
             list.add(biggestWord);
             System.out.println(biggestWord);
 
